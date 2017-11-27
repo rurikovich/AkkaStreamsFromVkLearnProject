ba71ccad646ece9604996f498ed6031f99530315
rurik <urrast@yandex.ru>
2017-11-27 16:50:07 +0300

    refactoring

--- src/main/scala/sample/stream/VkStreamingProcessing.scala
+++ src/main/scala/sample/stream/VkStreamingProcessing.scala
@@ -2,62 +2,56 @@ package sample.stream
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.OverflowStrategy.fail
-import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.scaladsl.Source
 import com.vk.api.sdk.client.VkApiClient
 import com.vk.api.sdk.client.actors.ServiceActor
 import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.streaming.clients.actors.StreamingActor
 import com.vk.api.sdk.streaming.clients.{StreamingEventHandler, VkStreamingApiClient}
 import com.vk.api.sdk.streaming.objects.StreamingCallbackMessage
+import com.vk.api.sdk.streaming.objects.responses.StreamingGetRulesResponse
+
+import scala.collection.JavaConversions._
 
 object VkStreamingProcessing {
+  val OK_CODE = 100
+
+  val appId = 6273957
+  val accessToken = "039164ab039164ab039164abfb03cedf0e00391039164ab598bc5f21d283fc052250a2d"
+  val clientSecret = "5rLSNuklraQD6mRjJXPx"
 
   def main(args: Array[String]): Unit = {
+
     implicit val system = ActorSystem("Sys")
     implicit val materializer = ActorMaterializer()
 
     val transportClient = new HttpTransportClient
     val vkClient = new VkApiClient(transportClient)
-
     val streamingClient = new VkStreamingApiClient(transportClient)
-
-    val appId = 6273957
-    val accessToken = "039164ab039164ab039164abfb03cedf0e00391039164ab598bc5f21d283fc052250a2d"
-    val clientSecret = "5rLSNuklraQD6mRjJXPx"
     val actor = new ServiceActor(appId, clientSecret, accessToken)
-
-    //Get streaming actor
     var getServerUrlResponse = vkClient.streaming.getServerUrl(actor).execute
-    val streamingActortor = new StreamingActor(getServerUrlResponse.getEndpoint, getServerUrlResponse.getKey)
+    val streamingActor = new StreamingActor(getServerUrlResponse.getEndpoint, getServerUrlResponse.getKey)
 
-    val tag = "3"
-    val value = "vk"
-
-    try
-      streamingClient.rules.add(streamingActortor, tag, value).execute
-    catch {
-      case e: Exception =>
-        e.printStackTrace()
+    val tag = "44"
+    val value = "привет"
+    val rules:StreamingGetRulesResponse = streamingClient.rules.get(streamingActor).execute()
+    if (!rules.getRules.toList.map(r=>r.getValue).contains(value)) {
+      streamingClient.rules.add(streamingActor, tag, value).execute
     }
 
-
-    val vkMessagesSource = Source.actorRef[String](200, fail)
-
-    val sunnySource = vkMessagesSource.filter(!_.isEmpty)
-
-    val ref = Flow[String]
+    val bufferSize = 10
+    val overflowStrategy = akka.stream.OverflowStrategy.backpressure
+    val queue = Source.queue[StreamingCallbackMessage](bufferSize, overflowStrategy)
+      .filter(_.getCode == OK_CODE)
+      .map(m => s"queue ${m.getEvent.getText}")
       .to(akka.stream.scaladsl.Sink foreach println)
-      .runWith(sunnySource)
+      .run()
 
-
-    streamingClient.stream.get(streamingActortor, new StreamingEventHandler() {
+    streamingClient.stream.get(streamingActor, new StreamingEventHandler() {
       override def handle(message: StreamingCallbackMessage): Unit = {
-        ref ! message.getEvent.getText
+        queue offer message
       }
     }).execute
 
-
   }
-
 }
